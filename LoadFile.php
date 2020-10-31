55b1f48ea9f5f91d2d086853a9d581ca17e6767b
procsitehcom2009 <37548022+procsitehcom2009@users.noreply.github.com>
2020-10-31 13:54:43 +0200

    Добавлен тип шифрования crc32

--- LoadFile.php
+++ LoadFile.php
@@ -7,19 +7,21 @@ class LoadFile {
     private $tmp_name;//Временное имя файла
     private $error;//Ошибки при загрузке
     private $size;//Размер файла
+    private $encryption_type;//Тип шифрования
     
-    public function __construct(string $path_dir,string $name,string $type,string $tmp_name,string $error,string $size) {
+    public function __construct(string $path_dir,string $name,string $type,string $tmp_name,string $error,string $size,string $encryption_type) {
         $this->path_dir=$path_dir;
         $this->name=$name;
         $this->type=$type;
         $this->tmp_name=$tmp_name;
         $this->error=$error;
-        $this->siza=$size;
+        $this->size=$size;
+        $this->encryption_type=$encryption_type;
     }
     
     public function get() {//Метод возвращает данные по файлу
         $this->upload();
-        $result = array ($this->get_name(), $this->get_size_file(), $this->get_extensions(), $this->get_md5_file());
+        $result = array ($this->get_name(), $this->get_size_file(), $this->get_extensions(), $this->get_encryption());
         $this->delete();
         return $result;
     }
@@ -32,8 +34,21 @@ class LoadFile {
         unlink($this->path_dir . $this->name);
     }
     
+    private function get_encryption(){//Метод проверяет тип шифрования и шифрует
+        if ($this->encryption_type=="crc32"){
+            return $this->get_crc32_file();
+        }
+        if ($this->encryption_type=="md5"){
+            return $this->get_md5_file();
+        }
+    }
+    
     private function get_md5_file(){//Метод возвращает hash md5 файла
         return md5_file($this->path_dir . $this->name);
+    }
+    
+    private function get_crc32_file(){//Метод возвращает hash crc32 файла
+        return crc32(file_get_contents($this->path_dir . $this->name));
     }
      
     private function get_size_file(){//Метод возвращает размер файла
